61d3782a65895f57ad81207f64bbb8425dfd19cf
Kazankov Dmitriy <kazankov.dmitry@gmail.com>
2021-11-11 11:50:12 +0300

    Initial Commit

---
+++ Hackerrank tasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hackerrank tasks
+//
+//  Created by Дмитрий Казанков on 11.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
